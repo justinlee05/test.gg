50e572c77b2bd15253fbd348da585a03433a9036
justinlee05 <justinlee05@naver.com>
2022-05-12 20:28:51 +0900

    [TEST] 훅 테스트
    실패. 내일 이어서

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getUserId } from "../data/api/getUserId";
 import { useChampMastery } from "../hooks/useChampMastery";
 
 const TestComponent = () => {
-  const [selector, getbyid] = useChampMastery();
+  const champState = useChampMastery();
+  const [data, setData] = useState();
 
   useEffect(() => {
-    console.log(selector, getbyid("이킵드"));
+    champState.setState.getList("이킵드");
+
+    setTimeout(() => {
+      console.log(champState.state)
+    }, 3000);
   }, []);
 
   return <></>;
