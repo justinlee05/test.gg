e1c2548911057501ec4bb0fc4eaf044ffce8be50
justinlee05 <justinlee05@naver.com>
2022-05-24 20:12:55 +0900

    RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 import RootRouter from "./router/rootRouter";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
-  const [queryClient] = useState(()=>new QueryClient());
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
         <GlobalStyle />
         <RootRouter />
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
