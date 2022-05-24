565e59e8f4ff8be924189c31dffb810f132f1371
justinlee05 <justinlee05@naver.com>
2022-05-24 20:14:46 +0900

    uri 추가
    티어 불러오기

--- src/data/api/Rioturi.ts
+++ src/data/api/Rioturi.ts
@@ -7,9 +7,13 @@ const ChampMasteryByLevel = (playerid: string) =>
   `/lol/champion-mastery/v4/scores/by-summoner/${playerid}`;
 const GetSummonerId = (playerName: string) =>
   `/lol/summoner/v4/summoners/by-name/${playerName}`;
+const GetLeagueEntry = (playerid:string) => 
+`/lol/league/v4/entries/by-summoner/${playerid}`
+
 export const rioturi = {
   champ_mastery_score: ChampMasteryByScore,
   champ_mastery_champ: ChampMasteryByChamp,
   champ_mastery_level: ChampMasteryByLevel,
   get_summoner_id: GetSummonerId,
+  get_league_entry:GetLeagueEntry
 };
