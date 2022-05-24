9b72d54d55a64fa825452d67e56236cc4f096155
justinlee05 <justinlee05@naver.com>
2022-05-24 20:15:19 +0900

    getUserId 리턴타입 정의 및 정리

--- src/data/api/getUserId.ts
+++ src/data/api/getUserId.ts
@@ -1,24 +1,23 @@
 import { RiotApiRequest } from "./default";
-import {  rioturi } from "./Rioturi";
+import { rioturi } from "./Rioturi";
 
 type IdResponseType = {
-  accountId: string; //string	Encrypted account ID. Max length 56 characters.
-  profileIconId: number; //int	ID of the summoner icon associated with the summoner.
-  revisionDate: number; //long	Date summoner was last modified specified as epoch milliseconds. The following events will update this timestamp: summoner name change, summoner level change, or profile icon change.
-  name: string; //string	Summoner name.
-  id: string; //string	Encrypted summoner ID. Max length 63 characters.
-  puuid: string; //string	Encrypted PUUID. Exact length of 78 characters.
-  summonerLevel: number; //long	Summoner level associated with the summoner.
+  data: {
+    accountId: string; //string	Encrypted account ID. Max length 56 characters.
+    profileIconId: number; //int	ID of the summoner icon associated with the summoner.
+    revisionDate: number; //long	Date summoner was last modified specified as epoch milliseconds. The following events will update this timestamp: summoner name change, summoner level change, or profile icon change.
+    name: string; //string	Summoner name.
+    id: string; //string	Encrypted summoner ID. Max length 63 characters.
+    puuid: string; //string	Encrypted PUUID. Exact length of 78 characters.
+    summonerLevel: number; //long	Summoner level associated with the summoner.
+  };
 };
 
 //유저의 닉네임을 통해 id값을 가져오는 함수입니다.
 
 export const getUserId = async (playerName: string) => {
-  try {
-    const response = await RiotApiRequest().get(rioturi.get_summoner_id(playerName));
-    console.log("res",response)
-    return response.data;
-  } catch (err) {
-    throw err;
-  }
+  const response:IdResponseType = await RiotApiRequest().get(
+    rioturi.get_summoner_id(playerName)
+  );
+  return response.data;
 };
